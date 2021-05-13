f52eaa6057eb09e106ad7c5c3c58f7f74d4c27db
정재은 <jejung@growthsoft.co.kr>
2021-05-13 16:47:44 +0900

    Initial commit for squishy ui

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  asdfasf
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      default: '메롱'
+    }
+  },
+  setup: () => {
+    const count = ref(0)
+
+    const ddd = 1
+    return { count }
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
